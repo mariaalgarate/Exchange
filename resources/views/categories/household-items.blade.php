cc789d2c34aa1be45c525dfdb9b55125e6aaf846
mariaalgarate <168902509+mariaalgarate@users.noreply.github.com>
2024-05-24 22:21:47 +0200

    categories

--- resources/views/categories/household-items.blade.php
+++ resources/views/categories/household-items.blade.php
@@ -1,9 +1,12 @@
+@extends('layouts.app')
+
+@section('styles')
 <style>
     /* Estilos personalizados */
     .banner {
         position: relative;
         height: 300px;
-        background: url('../imgs/bannerHouseHoldItems.jpg') center/cover;
+        background: url('{{ asset('imgs/bannerHouseHoldItems.jpg') }}') center/cover;
         display: flex;
         align-items: center;
         justify-content: center;
@@ -28,68 +31,65 @@
         padding: 20px;
     }
 </style>
+@endsection
 
-
-@extends('layouts.app')
-        @section('content')
-        <main class="pb-4">
-            <div class="banner">
-                <div class="banner-overlay">
-                    <h1 class="text-overlay">Decora tu hogar para lograr una mayor comodidad</h1>
-                </div>
+@section('content')
+<main class="pb-4">
+    <div class="banner">
+        <div class="banner-overlay">
+            <h1 class="text-overlay">Decora tu hogar para lograr una mayor comodidad</h1>
+        </div>
+    </div>
+    
+    <div class="container-fluid mt-5">
+        <div class="row mt-4">
+            <div class="col-md-3 category-menu">
+                <!-- Aquí va el menú de categorías -->
+                <h2>Categorías</h2>
+                <ul class="list-unstyled" style="line-height: 30px;">
+                    <li><a href="#" class="link-category">Decoración</a></li>
+                    <li><a href="#" class="link-category">Organización y almacenamiento</a></li>
+                    <li><a href="#" class="link-category">Jardín y exterior</a></li>
+                    <li><a href="#" class="link-category">Baño</a></li>
+                    <li><a href="#" class="link-category">Iluminación</a></li>
+                    <li><a href="#" class="link-category">Sofás y sillones</a></li>
+                    <li><a href="#" class="link-category">Escritorios y mesas de estudio</a></li>
+                    <li><a href="#" class="link-category">Camas y cabeceras</a></li>
+                    <li><a href="#" class="link-category">Espejos</a></li>
+                </ul>
             </div>
+            <div class="col-md-9 product-section mt-5">
+                <!-- Lo más vendido -->
+                <h2>Lo más vendido (0-20€)</h2>
             
-            <div class="container-fluid mt-5">
-                <div class="row mt-4">
-                    <div class="col-md-3 category-menu">
-                        <!-- Aquí va el menú de categorías -->
-                        <h2>Categorías</h2>
-                        <ul class="list-unstyled" style="line-height: 30px;">
-                            <li><a href="#" class="link-category">Decoración</a></li>
-                            <li><a href="#" class="link-category">Organización y almacenamiento</a></li>
-                            <li><a href="#" class="link-category">Jardín y exterior</a></li>
-                            <li><a href="#" class="link-category">Baño</a></li>
-                            <li><a href="#" class="link-category">Iluminación</a></li>
-                            <li><a href="#" class="link-category">Sofás y sillones</a></li>
-                            <li><a href="#" class="link-category">Escritorios y mesas de estudio</a></li>
-                            <li><a href="#" class="link-category">Camas y cabeceras</a></li>
-                            <li><a href="#" class="link-category">Espejos</a></li>
-                            
-                        </ul>
-                    </div>
-                    <div class="col-md-9 product-section mt-5">
-                        <!-- Lo más vendido -->
-                        <h2>Lo más vendido (0-20€)</h2>
-                    
-                        <!-- Filtrar la colección para obtener solo productos <= 20€ -->
-                        @php
-                            $productosMenoresA20 = $productos->filter(function ($producto) {
-                                return $producto->precio_unitario <= 20;
-                            });
-                        @endphp
-                    
-                        @if($productosMenoresA20->isEmpty())
-                            <p>No hay productos en esta categoría por menos de 20€.</p>
-                        @else
-                            <div class="row mt-4">
-                                @foreach ($productosMenoresA20 as $producto)
-                                <div class="col-md-3">
-                                    <div class="card">
-                                        <img src="{{ asset('storage/' . $producto->imagen) }}" class="card-img-top" alt="{{ $producto->nombre }}">
-                                        <div class="card-body">
-                                            <h5 class="card-title">{{ $producto->nombre }}</h5>
-                                            <p class="card-text">{{ $producto->descripcion }}</p>
-                                            <p class="card-text">Precio: {{ number_format($producto->precio_unitario, 2) }}€</p>
-                                            <a href="{{ route('show', ['id' => $producto->id]) }}" class="btn btn-primary">Ver Producto</a>
-                                        </div>
-                                    </div>
+                <!-- Filtrar la colección para obtener solo productos <= 20€ -->
+                @php
+                    $productosMenoresA20 = $productos->filter(function ($producto) {
+                        return $producto->precio_unitario <= 20;
+                    });
+                @endphp
+            
+                @if($productosMenoresA20->isEmpty())
+                    <p>No hay productos en esta categoría por menos de 20€.</p>
+                @else
+                    <div class="row mt-4">
+                        @foreach ($productosMenoresA20 as $producto)
+                        <div class="col-md-3">
+                            <div class="card">
+                                <img src="{{ asset($producto->imagen) }}" class="card-img-top" alt="{{ $producto->nombre }}">
+                                <div class="card-body">
+                                    <h5 class="card-title">{{ $producto->nombre }}</h5>
+                                    <p class="card-text">{{ $producto->descripcion }}</p>
+                                    <p class="card-text">Precio: {{ number_format($producto->precio_unitario, 2) }}€</p>
+                                    <a href="{{ route('show', ['id' => $producto->id]) }}" class="btn btn-primary">Ver Producto</a>
                                 </div>
-                                @endforeach
                             </div>
-                        @endif
+                        </div>
+                        @endforeach
                     </div>
-                </div>
+                @endif
             </div>
         </div>
-    @endsection
-            
+    </div>
+</main>
+@endsection
