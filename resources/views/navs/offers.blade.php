fe4c6c0f705bf2bcb3c8d746524af96b76668480
mariaalgarate <168902509+mariaalgarate@users.noreply.github.com>
2024-05-24 21:12:23 +0200

    Update offers.blade.php

--- resources/views/navs/offers.blade.php
+++ resources/views/navs/offers.blade.php
@@ -26,6 +26,41 @@ $productos = Producto::all();
         height: 100%;
         background-color: rgba(0, 0, 0, 0.5);
     }
+
+    .product-grid {
+            display: flex;
+            flex-wrap: wrap;
+            gap: 20px;
+            justify-content: space-between; /* Distribuye espacio uniformemente */
+        }
+
+        .product {
+            flex: 1 1 calc(33.333% - 20px); /* Toma 33.333% del ancho menos 20px de margen */
+            box-sizing: border-box;
+            margin: 10px 0;
+            padding: 20px;
+            border: 1px solid #ccc;
+            border-radius: 10px;
+            text-align: center;
+        }
+
+        .product img {
+            max-width: 100%;
+            height: auto;
+            border-radius: 5px;
+        }
+
+        @media (max-width: 768px) {
+            .product {
+                flex: 1 1 calc(50% - 20px); /* Toma 50% del ancho menos 20px de margen */
+            }
+        }
+
+        @media (max-width: 576px) {
+            .product {
+                flex: 1 1 100%; /* Toma 100% del ancho en pantallas pequeñas */
+            }
+        }
 </style>
 <main class="pb-4">
     <div class="banner">
